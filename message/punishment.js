737e43f60bd02ab23f7606989d04e61d5b888e4b
Glitch (werewolf-glitch-test) <none>
2020-03-21 05:32:49 +0000

    🌇🚣 Updated with Glitch

---
+++ message/punishment.js
@@ -0,0 +1,17 @@
+// di + punishment
+// meng + punishment
+const punishment = [
+  "gantung",
+  "gelitik",
+  "bakar",
+  "jual",
+  "pacung",
+  "gebuk",
+  "infeksi corona",
+  "suntik mati",
+  "kubur hiduphidup",
+  "tenggelamkan",
+  "tampol"
+];
+
+module.exports = punishment;
