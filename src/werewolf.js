2446c59a020d3aa73e14043f7e9ca41f046decf9
Glitch (werewolf-glitch-test) <none>
2020-04-15 06:17:05 +0000

    Exporting to EriecTanijaya/werewolf ./src/werewolf.js:2046517/145

--- src/werewolf.js
+++ src/werewolf.js
@@ -3611,9 +3611,7 @@ module.exports = {
     // jumlah ww dibatasin 75% dari badNeedCount Quota
     let werewolfNeedCount = Math.round((75 / 100) * badNeedCount);
 
-    // plus 1 reserve untuk werewolf
-    // ini kalau jumlah ww yang dibutuhkan lebih besar dari jumlah team ww yg ada
-    let maxWerewolfCount = werewolfTeam.length + 1;
+    let maxWerewolfCount = 4;
     if (werewolfNeedCount > maxWerewolfCount) {
       werewolfNeedCount = maxWerewolfCount;
     }
