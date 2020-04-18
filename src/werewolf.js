0eace3aa8d6baca8369cb90f4c467c60d0f8f41a
Glitch (werewolf-glitch-test) <none>
2020-04-18 15:40:15 +0000

    Exporting to EriecTanijaya/werewolf ./src/werewolf.js:2046517/280

--- src/werewolf.js
+++ src/werewolf.js
@@ -356,12 +356,12 @@ module.exports = {
       this.addPlayer(newPlayer);
 
       //cp
-      for (let i = 0; i < 8; i++) {
-        let dummy = JSON.parse(JSON.stringify(this.user_session));
-        dummy.name += " " + helper.getRandomInt(1, 99);
-        let newPlayer = this.createNewPlayer(dummy);
-        this.addPlayer(newPlayer);
-      }
+      // for (let i = 0; i < 8; i++) {
+      //   let dummy = JSON.parse(JSON.stringify(this.user_session));
+      //   dummy.name += " " + helper.getRandomInt(1, 99);
+      //   let newPlayer = this.createNewPlayer(dummy);
+      //   this.addPlayer(newPlayer);
+      // }
 
       let text = "💡 " + this.user_session.name + " berhasil bergabung!";
       return this.replyFlex(flex_text, [text, remindText]);
@@ -641,19 +641,19 @@ module.exports = {
     ///werewolf harus selalu ada
     let players = this.group_session.players;
     let playersLength = players.length;
-    //let roles = this.getRandomRoleSet(playersLength); //cp
+    let roles = this.getRandomRoleSet(playersLength); //cp
 
     /// test specific role
-    let roles = [
-      "vampire",
-      "doctor",
-      "doctor",
-      "bodyguard",
-      "vampire-hunter",
-      "serial-killer",
-      "escort",
-      "bodyguard"
-    ];
+    // let roles = [
+    //   "vampire",
+    //   "doctor",
+    //   "doctor",
+    //   "bodyguard",
+    //   "vampire-hunter",
+    //   "serial-killer",
+    //   "escort",
+    //   "bodyguard"
+    // ];
 
     /// hax for exe
     let exeIndex = -1;
@@ -817,14 +817,14 @@ module.exports = {
     this.runTimer();
 
     //cp
-    let playersWithRole = this.group_session.players.map(i => {
-      return {
-        name: i.name,
-        roleName: i.role.name
-      };
-    });
+//     let playersWithRole = this.group_session.players.map(i => {
+//       return {
+//         name: i.name,
+//         roleName: i.role.name
+//       };
+//     });
 
-    console.table(playersWithRole);
+//     console.table(playersWithRole);
 
     if (flex_texts) {
       return this.replyFlex(flex_texts, null, newFlex_text);
@@ -3291,7 +3291,7 @@ module.exports = {
         item.message += werewolfAnnouncement;
       }
 
-      console.log(`pesan ${item.name} (${item.role.name}) : ${item.message}`); //cp
+      // console.log(`pesan ${item.name} (${item.role.name}) : ${item.message}`); //cp
 
       /// journal , keep this below any special Announcement
       if (item.status === "alive" && item.message !== "") {
