7e5a345dce41d72d92c23ce15310beabcec4d6b5
Glitch (werewolf-glitch-test) <none>
2020-04-24 09:20:04 +0000

    Exporting to EriecTanijaya/werewolf ./src/reset.js:2046517/634 ./server.js:2046517/252

--- src/reset.js
+++ src/reset.js
@@ -2,7 +2,11 @@ const dbClient = require("/app/src/databaseClient");
 
 // Reset User Points
 function resetUsersPoint() {
-  
+  let query = `UPDATE PlayerStats SET points = 0;`;
+  dbClient.query(query).then(() => {
+    let date = new Date();
+    console.log(`Done reset data on ${date}`);
+  })
 }
 
 module.exports = {
