63360c7bb24039e5f44eac4a738eafbe6aa91ccf
Glitch (werewolf-glitch-test) <none>
2020-04-24 04:54:27 +0000

    👑👄 Checkpoint ./src/databaseClient.js:2046517/595 ./src/database.js:2046517/6525

--- src/database.js
+++ src/database.js
@@ -5,63 +5,67 @@ const dbClient = require("/app/src/databaseClient");
 function getAllUserData(team, cb) {
   const users = [];
   let pending = 0;
-  
+  // dbClient.query('DROP TABLE IF EXISTS PlayerStats');
   let query = `
     SELECT * FROM PlayerStats
   `;
-  
-  dbClient.query(query)
-    .then(db => {
-      console.log(db);
-  })
-  
-//   fs.readdir(baseUserPath, (err, list) => {
-//     if (err) throw err;
-//     pending = list.length;
-//     list.forEach((item, index) => {
-//       if (item.includes("user")) {
-//         fs.readFile(baseUserPath + item, (err, data) => {
-//           let rawUser = JSON.parse(data);
 
-//           let stats = {
-//             villager: rawUser.villagerStats,
-//             werewolf: rawUser.werewolfStats,
-//             vampire: rawUser.vampireStats,
-//             jester: rawUser.jesterStats,
-//             serialKiller: rawUser.serialKillerStats,
-//             arsonist: rawUser.arsonistStats,
-//             survivor: rawUser.survivorStats,
-//             executioner: rawUser.executionerStats
-//           };
+  dbClient.query(query).then(db => {
+    console.log(db.rows);
+    let user = {
+      id: db.rows[0],
+      name: db.rows[1],
+      
+    }
+  });
 
-//           let result = calculateWinLose(team, stats);
-//           let totalGame = result.win + result.lose;
-//           let winRate = Math.floor((result.win / totalGame) * 100);
-//           if (isNaN(winRate)) {
-//             winRate = 0;
-//           }
+  //   fs.readdir(baseUserPath, (err, list) => {
+  //     if (err) throw err;
+  //     pending = list.length;
+  //     list.forEach((item, index) => {
+  //       if (item.includes("user")) {
+  //         fs.readFile(baseUserPath + item, (err, data) => {
+  //           let rawUser = JSON.parse(data);
 
-//           let user = {
-//             id: rawUser.id,
-//             name: rawUser.name,
-//             points: rawUser.points,
-//             totalGame: totalGame,
-//             winRate: winRate + "%"
-//           };
+  //           let stats = {
+  //             villager: rawUser.villagerStats,
+  //             werewolf: rawUser.werewolfStats,
+  //             vampire: rawUser.vampireStats,
+  //             jester: rawUser.jesterStats,
+  //             serialKiller: rawUser.serialKillerStats,
+  //             arsonist: rawUser.arsonistStats,
+  //             survivor: rawUser.survivorStats,
+  //             executioner: rawUser.executionerStats
+  //           };
 
-//           if (team) {
-//             let points = result.win * 5 + result.lose;
-//             user.points = points;
-//           }
+  //           let result = calculateWinLose(team, stats);
+  //           let totalGame = result.win + result.lose;
+  //           let winRate = Math.floor((result.win / totalGame) * 100);
+  //           if (isNaN(winRate)) {
+  //             winRate = 0;
+  //           }
 
-//           users.push(user);
-//           if (pending === index + 1) {
-//             cb(users);
-//           }
-//         });
-//       }
-//     });
-//   });
+  //           let user = {
+  //             id: rawUser.id,
+  //             name: rawUser.name,
+  //             points: rawUser.points,
+  //             totalGame: totalGame,
+  //             winRate: winRate + "%"
+  //           };
+
+  //           if (team) {
+  //             let points = result.win * 5 + result.lose;
+  //             user.points = points;
+  //           }
+
+  //           users.push(user);
+  //           if (pending === index + 1) {
+  //             cb(users);
+  //           }
+  //         });
+  //       }
+  //     });
+  //   });
 }
 
 function calculateWinLose(team, stats) {
--- src/databaseClient.js
+++ src/databaseClient.js
@@ -9,17 +9,17 @@ dbClient
   .then(db => {
     console.log("database connected");
   
-  dbClient.query(`CREATE TABLE IF NOT EXISTS PlayerStats (id text, name text, points intege);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS VillagerStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS WerewolfStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS VampireStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS JesterStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS SerialKillerStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS ArsonistStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS SurvivorStats (playerId, win int, lose int);`);
-  dbClient.query(`CREATE TABLE IF NOT EXISTS ExecutionerStats (playerId, win int, lose int);`);
+  dbClient.query('CREATE TABLE IF NOT EXISTS PlayerStats (id text, name text, points integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS VillagerStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS WerewolfStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS VampireStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS JesterStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS SerialKillerStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS ArsonistStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS SurvivorStats (playerId, win integer, lose integer);');
+  // dbClient.query('CREATE TABLE IF NOT EXISTS ExecutionerStats (playerId, win integer, lose integer);');
   
-  dbClient.query(`INSERT INTO PlayerStats (id, name, points) VALUES ('2060', 'Siapalah', 10);`);
+  //dbClient.query(`INSERT into PlayerStats (id, name, points) VALUES ('2060', 'Siapalah', 10);`);
   
   })
   .catch(err => {
