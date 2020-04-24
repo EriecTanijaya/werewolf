f630f6cd2fd7b33d91105ad4796e85cfaef34276
Glitch (werewolf-glitch-test) <none>
2020-04-24 09:20:05 +0000

    rework reset function

--- src/reset.js
+++ src/reset.js
@@ -1,37 +1,12 @@
-const baseUserPath = "/app/.data/users/";
-const fs = require("fs");
+const dbClient = require("/app/src/databaseClient");
 
 // Reset User Points
 function resetUsersPoint() {
-  fs.readdir(baseUserPath, (err, data) => {
-    if (err) throw err;
-    getUserPath(data);
-  });
-}
-
-function getUserPath(list) {
-  list.map((item, index) => {
-    if (item.includes("user")) {
-      getUserData(baseUserPath + item);
-    }
-  });
-  let date = new Date();
-  console.log(`Done reset data on ${date}`);
-}
-
-function getUserData(path) {
-  fs.readFile(path, (err, data) => {
-    let user = JSON.parse(data);
-    user.points = 0;
-    let editedUser = JSON.stringify(user, null, 2);
-    updateUserData(path, editedUser);
-  });
-}
-
-function updateUserData(path, editedUser) {
-  fs.writeFile(path, editedUser, err => {
-    if (err) throw err;
-  });
+  let query = `UPDATE PlayerStats SET points = 0;`;
+  dbClient.query(query).then(() => {
+    let date = new Date();
+    console.log(`Done reset data on ${date}`);
+  })
 }
 
 module.exports = {
