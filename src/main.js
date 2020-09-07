545b21e38653e8991e69f83e08fda3490dd2f0c4
Glitch (werewolf-glitch-test) <none>
2020-09-07 05:47:50 +0000

    linting shit

--- src/main.js
+++ src/main.js
@@ -3983,7 +3983,7 @@ const endGame = (flex_texts, whoWin) => {
 
     if (roleTeam === whoWin) {
       table_data.push("win");
-      database.update(players[i].id, "win");
+      database.update(players[i].id, "win", this.group_session.mode);
     } else {
       /// check the win condition of some role
       if (roleName === "jester") {
@@ -3991,17 +3991,18 @@ const endGame = (flex_texts, whoWin) => {
       } else if (roleName === "survivor") {
         handleSurvivorWin(i, table_data, surviveTeam);
       } else if (roleName === "amnesiac") {
-        handleAmnesiacWin(i, table_data, surviveTeam);
+        table_data.push("lose");
+        database.update(players[i].id, "lose", this.group_session.mode);
       } else if (roleName === "executioner") {
         handleExecutionerWin(i, table_data, surviveTeam);
       } else if (roleName === "guardian-angel") {
         handleGuardianAngelWin(i, table_data, surviveTeam);
       } else if (whoWin === "draw") {
         table_data.push("draw");
-        database.update(players[i].id, "draw");
+        database.update(players[i].id, "draw", this.group_session.mode);
       } else {
         table_data.push("lose");
-        database.update(players[i].id, "lose");
+        database.update(players[i].id, "lose", this.group_session.mode);
       }
     }
 
@@ -4062,10 +4063,10 @@ const handleGuardianAngelWin = (index, table_data, surviveTeam) => {
   if (this.group_session.players[targetIndex].status === "alive") {
     table_data.push("win");
     surviveTeam.push("guardian angel 😇");
-    database.update(this.group_session.players[index].id, "win");
+    database.update(this.group_session.players[index].id, "win", this.group_session.mode);
   } else {
     table_data.push("lose");
-    database.update(this.group_session.players[index].id, "lose");
+    database.update(this.group_session.players[index].id, "lose", this.group_session.mode);
   }
 };
 
@@ -4073,25 +4074,21 @@ const handleExecutionerWin = (index, table_data, surviveTeam) => {
   if (this.group_session.players[index].role.isTargetLynched) {
     table_data.push("win");
     surviveTeam.push("executioner 🪓");
-    database.update(this.group_session.players[index].id, "win");
+    database.update(this.group_session.players[index].id, "win", this.group_session.mode);
   } else {
     table_data.push("lose");
-    database.update(this.group_session.players[index].id, "lose");
+    database.update(this.group_session.players[index].id, "lose", this.group_session.mode);
   }
-};
-
-const handleAmnesiacWin = (index, table_data, surviveTeam) => {
-  database.update(this.group_session.players[index].id, "lose");
 };
 
 const handleSurvivorWin = (index, table_data, surviveTeam) => {
   if (this.group_session.players[index].status === "alive") {
     table_data.push("win");
     surviveTeam.push("survivor 🏳️");
-    database.update(this.group_session.players[index].id, "win");
+    database.update(this.group_session.players[index].id, "win", this.group_session.mode);
   } else {
     table_data.push("lose");
-    database.update(this.group_session.players[index].id, "lose");
+    database.update(this.group_session.players[index].id, "lose", this.group_session.mode);
   }
 };
 
@@ -4099,10 +4096,10 @@ const handleJesterWin = (index, table_data, surviveTeam) => {
   if (this.group_session.players[index].role.isLynched) {
     table_data.push("win");
     surviveTeam.push("jester 🤡");
-    database.update(this.group_session.players[index].id, "win");
+    database.update(this.group_session.players[index].id, "win", this.group_session.mode);
   } else {
     table_data.push("lose");
-    database.update(this.group_session.players[index].id, "lose");
+    database.update(this.group_session.players[index].id, "lose", this.group_session.mode);
   }
 };
 
@@ -4869,7 +4866,6 @@ const joinCommand = () => {
 
   this.group_session.players.push(newPlayer);
 
-  //cp
   database.add(this.user_session);
 
   let reminder = "⏳ Sisa waktu ";
@@ -4960,7 +4956,6 @@ const newCommand = () => {
   const newPlayer = createNewPlayer(this.user_session);
   this.group_session.players.push(newPlayer);
 
-  //cp
   database.add(this.user_session);
 
   if (process.env.TEST === "true") {
