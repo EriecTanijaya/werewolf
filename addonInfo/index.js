e18b6534841abf20a555d6913323f149888c273f
Glitch (werewolf-glitch-test) <none>
2020-09-18 08:28:19 +0000

    info : add addonInfo

--- addonInfo/index.js
+++ addonInfo/index.js
@@ -6,7 +6,7 @@ module.exports = {
   afk: {
     headerText: "💤 Away From Kibord",
     bodyText:
-      "Moderator akan mengeliminasi kamu jika ketahuan AFK, yang afk itu adalah yang tidak ada ikut diskusi di grup, tidak vote, tidak menggunakan skill."
+      "Moderator akan mengeliminasi kamu jika ketahuan AFK, yang afk itu adalah yang tidak ada ikut diskusi di grup, tidak vote, tidak menggunakan skill secara berturut-turut"
   },
   mafia: {
     headerText: "🤵 Mafia",
@@ -17,5 +17,15 @@ module.exports = {
     headerText: "🕵️ Note Investigator",
     bodyText:
       "- targetmu memiliki senjata!\nvigilante, veteran, mafioso\n\n- targetmu berurusan dengan mayat!\nretributionist\n\n- targetmu suka menutup diri!\nsurvivor, vampire-hunter, psychic, amnesiac\n\n- targetmu mengetahui rahasia terbesarmu!\nspy, guardian-angel\n\n- targetmu menunggu waktu yang tepat untuk beraksi!\nsheriff, executioner, werewolf\n\n- targetmu mungkin tidak seperti yang dilihat!\nframer, vampire, jester\n\n- targetmu diam dalam bayangan!\nlookout, juggernaut\n\n- targetmu ahli dalam mengganggu yang lain!\nescort, consort\n\n- targetmu berlumuran darah!\ndoctor, serial-killer, disguiser\n\n- targetmu memiliki rahasia yang terpendam!\ninvestigator, consigliere, mayor, tracker, plaguebearer\n\n- targetmu tidak takut kotor!\nbodyguard, godfather, arsonist\n\n\n📜 Note :\n- orang yang di frame hasilnya role framer, vampire, atau jester\n- orang yang di sirami bensin oleh arsonist hasilnya bodyguard, godfather, arsonist"
+  },
+  bot: {
+    headerText: "🤖 Bots",
+    bodyText:
+      "Pemain yang bisa mengisi suatu game. Bot bisa menggunakan skill dan vote, tapi masih bodoh. Bot menggunakan skill secara random. Guide nya bisa di cek di '/info note bot'"
+  },
+  "note-bot": {
+    headerText: "🤖 Note Bots",
+    bodyText:
+      "- 'vote 3' : untuk menggiring bot, untuk vote pemain ke 3 dari '/players' list\n\n- 'info' : untuk meminta suatu info dari bot yang ada, bisa di respon, bisa tidak\n\n- '<nama-bot> jahat' : untuk menuduh suatu bot, bot bisa respon\n\n- '<nama-bot> role' : untuk meminta nama role\n\nNote : \nBot bisa berbohong, seperti memberi info palsu, bot juga belum tentu merespon giringan vote, atau bisa saja random vote"
   }
 };
