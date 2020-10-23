b60a4ece32f7cd73f14c7dfdd5ee1fabef1803dd
Glitch (werewolf-glitch-test) <none>
2020-10-23 13:08:55 +0000

    cleanup

--- server.js
+++ server.js
@@ -11,38 +11,6 @@ const config = {
   channelSecret: process.env.CHANNEL_SECRET
 };
 
-/// measure request time
-// Thanks to https://slao.io/blog/posts/request-duration/
-const getDurationInMilliseconds = start => {
-  const NS_PER_SEC = 1e9;
-  const NS_TO_MS = 1e6;
-  const diff = process.hrtime(start);
-
-  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
-};
-
-app.use((req, res, next) => {
-  if (process.env.TEST === "true") console.log(`${req.method} ${req.originalUrl} [STARTED]`);
-
-  const start = process.hrtime();
-
-  res.on("finish", () => {
-    const durationInMilliseconds = getDurationInMilliseconds(start);
-    let text = `${req.method} ${req.originalUrl} [FINISHED] `;
-    text += `${durationInMilliseconds.toLocaleString()} ms`;
-    if (process.env.TEST === "true") console.log(text);
-  });
-
-  res.on("close", () => {
-    const durationInMilliseconds = getDurationInMilliseconds(start);
-    let text = `${req.method} ${req.originalUrl} [CLOSED] `;
-    text += `${durationInMilliseconds.toLocaleString()} ms`;
-    if (process.env.TEST === "true") console.log(text);
-  });
-
-  next();
-});
-
 app.use("/callback", line.middleware(config));
 
 app.get("/", (req, res) => res.sendStatus(200));
