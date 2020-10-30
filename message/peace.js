0dea94a9f66d14920313549cb8fac6cc6ab0199a
Glitch (werewolf-glitch-test) <none>
2020-10-30 02:01:48 +0000

    Peace message : add new text

--- message/peace.js
+++ message/peace.js
@@ -1,8 +1,10 @@
 const peace = [
-  "🛏️ Tadi malam terlewati tanpa ada pembunuhan", 
+  "🛏️ Tadi malam terlewati tanpa ada pembunuhan",
   "🛏️ Pagi ini tidak ada bertambahnya orang mati",
   "☠️ Malaikat maut sia sia datang ke kota Bedburg, gak ada yang mati",
-  "⚰️ Pagi ini tumben gak ada yang mati"
+  "⚰️ Pagi ini tumben gak ada yang mati",
+  "☠️ Malaikat maut ketiduran hari ini, jadi gak ada yang mati",
+  "⚰️ Daftar orang mati hari ini = kosong"
 ];
 
 module.exports = peace;
